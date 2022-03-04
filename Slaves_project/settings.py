6ed7cf08123f756a8f179fc056d34243ea45dbde
moonvolk4 <84585564+moonvolk4@users.noreply.github.com>
2022-03-04 20:01:21 +0300

    Update settings.py
    Updated: css added
    Don't forget to import "os' library!

--- Slaves_project/settings.py
+++ Slaves_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
