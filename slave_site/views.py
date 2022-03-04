d7239f9f823959ae8caa8733c5a0d2e4768bd835
LikoyakaTemik <92818915+LikoyakaTemik@users.noreply.github.com>
2022-03-04 19:51:48 +0300

    Added database
    Added database with using of SQLite.
    Structure:
    Special class: Database_construction, where was realized staticmethod creating_tables, which responsible for creating of tables. If result is error --> pass

--- slave_site/views.py
+++ slave_site/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
-
+import sqlite3 as sq
+db = sq.connect("DB")
+class Database_construction:
+    @staticmethod
+    def creating_tables():
+        try:
+            db.execute("CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                       " nickname TEXT,"
+                       " mail TEXT,"
+                       " password TEXT)")
+            db.execute("CREATE TABLE products(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                       " label TEXT,"
+                       " likes INTEGER,"
+                       " id_category TEXT,"
+                       " price INTEGER, "
+                       " url_img TEXT)")
+            db.execute("CREATE TABLE users_products(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                       " id_product INTEGER,"
+                       " id_user INTEGER)")
+            db.execute("CREATE TABLE categories(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                       "name TEXT)")
+            db.execute("CREATE TABLE likes(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                       " id_user INTEGER,"
+                       " id_category INTEGER,"
+                       " amount INTEGER)")
+            db.execute("CREATE TABLE shopping_cart(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                       " id_user INTEGER,"
+                       " id_product INTEGER,"
+                       " amount INTEGER)")
+        except:
+            print("Таблицы уже созданы!")
 def index_page(request):
     context = {}
+    
     return render(request,"index.html", context)
