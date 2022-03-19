c42f29a2c37a5612a69ac2bd97ac5b1f7d9028a7
moonvolk4 <84585564+moonvolk4@users.noreply.github.com>
2022-03-19 16:40:10 +0300

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from slave_site import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.index_page),
+    path("catalog/", views.catalog_page),
+    path("login/", views.login_page),
+]
