b1fe3f4898cfc3dbd258e7f75cc9098fbc4f2b59
LikoyakaTemik <92818915+LikoyakaTemik@users.noreply.github.com>
2022-04-08 20:56:19 +0300

    Authorization
    Added authorization

--- slave_site/models.py
+++ slave_site/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
--- slave_site/views.py
+++ slave_site/views.py
@@ -6,16 +6,14 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+
 import sqlite3 as sq
 id_user = 0
+nickname = ["Anonymous"]
 class Database_construction:
     @staticmethod
     def creating_tables(db):
         try:
-            db.execute("CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                       " nickname TEXT,"
-                       " mail TEXT,"
-                       " password TEXT)")
             db.execute("CREATE TABLE products(id INTEGER PRIMARY KEY AUTOINCREMENT,"
                        " label TEXT,"
                        " likes INTEGER,"
@@ -39,7 +37,7 @@ class Database_construction:
             print("Таблицы уже созданы!")
 
 def adding_product(request):
-    db = sq.connect("DB")
+    db = sq.connect("db.sqlite3")
     Database_construction.creating_tables(db)
     if request.method == "POST":
         id_product = int(request.POST.get('id'))
@@ -70,70 +68,59 @@ def catalog_page(request):
 
 def enter(request):
     """display page of enter, executing of code of backend of log in"""
-    database = sq.connect("mesbase.sqlite3", timeout=10)
-    try:
-        database.execute(
-            "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, nickname TEXT, password TEXT)")
-    except:
-        pass
+    db = sq.connect("db.sqlite3")
+    Database_construction.creating_tables(db)
     ###подключение к БД
     if (request.method == "POST"):
-        nick = request.POST.get("nick")###вместо nick название атрибута name тега input
-        password = str(request.POST.get("password")) ###вместо paswword название атрибута name тега input
+        email = str(request.POST.get("exampleInputEmail1"))
+        password = str(request.POST.get("exampleInputPassword1"))
         info = "nonclear"
-        try:
-            info = (database.execute("SELECT nickname FROM users WHERE nickname='" + nick + "'")).fetchall()
-        except:
-            pass
+
+        info = (db.execute("SELECT 'auth_user'.'email' FROM 'auth_user' WHERE ('auth_user'.'email'='" + email + "')")).fetchall()
+
+
         ##проверка на наличие ячейки
         if(info == []):
-            database.close()
-            return render(request, 'errorpas.html')
+            print(info)
+            print(email)
+            print("NONE_EMAIL")
+            db.close()
+            return render(request, 'error.html') ##Нужно сделать
         else:
-            truepassword = ((database.execute("SELECT password from users WHERE nickname='" + nick + "'")).fetchall())[0][0]
-            truepasw_res = ""
-            ###запрос шифрованного пароля из БД
-            for i in range(len(truepassword)):
-                truepasw_res = truepasw_res + chr(ord(truepassword[i]) - 10)
-            truepassword = truepasw_res
-            ###расшифровка пароля
+            truepassword = str(((db.execute("SELECT 'auth_user'.'password' from 'auth_user' WHERE ('auth_user'.'email'='" + email + "')")).fetchall())[0][0])
             if(truepassword == password):
                 logging = ""
-                if (nick == "Anonymous"):
+                if (email == "Anonymous"):
                     logging = "Войти"
                 else:
                     logging = "Выйти"
-                tranport_data = {"user": nick, "logging": logging}
-                nickname[0] = nick
-                database.close()
-
+                tranport_data = {"username": email, "logging": logging}
+                nickname[0] = ((db.execute("SELECT 'auth_user'.'username' from 'auth_user' WHERE ('auth_user'.'email'='" + email + "')")).fetchall())[0][0]
+                db.close()
                 return redirect("http://127.0.0.1:8000", logging = "Выйти")
             else:
-                database.close()
-                return render(request, 'errorpas.html')
-            ###сравнивание: если пароли совпадают -> логин, иначе ошибка
+                db.close()
+                print("FALSE_PASSWORD")
+                return render(request, 'error.html')
+            ###сравнивание: есл и пароли совпадают -> логин, иначе ошибка
 
 
     elif (request.method == "GET"):
-        database.close()
+        db.close()
         return render(request, 'login.html')
 
 
 def registration(request):
-    database = sq.connect("mesbase.sqlite3", timeout=10)
     info = []
-    try:
-        database.execute(
-            "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, nickname TEXT, password TEXT)")
-    except:
-        pass
+    db = sq.connect("db.sqlite3")
+    Database_construction.creating_tables(db)
 
     if(request.method=="POST"):
         nick = request.POST.get("nick")
         pasw = request.POST.get("password")
         info = "nonclear"
         try:
-            info = (database.execute("SELECT * FROM users WHERE nickname='" + request.POST.get("nick") + "'")).fetchall()
+            info = (db.execute("SELECT * FROM users WHERE nickname='" + request.POST.get("nick") + "'")).fetchall()
         except:
             pass
 
@@ -146,26 +133,32 @@ def registration(request):
             pasw = pasw_res
 
             try:
-                database.execute("CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, nickname TEXT, password TEXT)")
+                db.execute("CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, nickname TEXT, password TEXT)")
             except:
                 pass
-            with database:
-                database.execute("INSERT INTO users(nickname, password) VALUES ('" + nick + "', '" + pasw + "')")
-                database.commit()
-            database.close()
+            with db:
+                db.execute("INSERT INTO users(nickname, password) VALUES ('" + nick + "', '" + pasw + "')")
+                db.commit()
+            db.close()
             return render(request, 'congratulateyou.html')
         else:
-            database.close()
+            db.close()
             return render(request, 'error.html')
 
 
     elif(request.method=="GET"):
         tranport_data = {"user": nickname[0]}
-        database.close()
+        db.close()
         return render(request, 'Registration.html', tranport_data)
 
 def index_page(request):
-    db = sq.connect("DB")
-    Database_construction.creating_tables(db)
-    db.close()
-    return render(request,"Main_page.html")
+    if request.method == "POST":
+        db = sq.connect("db.sqlite3")
+        Database_construction.creating_tables(db)
+        db.close()
+        return render(request, "index.html")
+    elif request.method == "GET":
+        db = sq.connect("db.sqlite3")
+        Database_construction.creating_tables(db)
+        db.close()
+        return render(request, "index.html")
