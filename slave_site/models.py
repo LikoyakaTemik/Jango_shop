1f9e2ae0d870af27e3d8529640848876b218f324
ivanbux <99982877+ivanbux@users.noreply.github.com>
2022-04-29 18:24:34 +0300

    fixed registration

--- slave_site/models.py
+++ slave_site/models.py
@@ -2,10 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    nickname = models.CharField(max_length=20)
-    email = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+
 
 class Product(models.Model):
     label = models.CharField(max_length=20)
