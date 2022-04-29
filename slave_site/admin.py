22f87014ca053535550c1646eca3d16143993a17
ivanbux <99982877+ivanbux@users.noreply.github.com>
2022-04-29 18:23:20 +0300

    fixed registration

--- slave_site/admin.py
+++ slave_site/admin.py
@@ -1,10 +1,1 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
-
-
-from .models import CustomUser
 
