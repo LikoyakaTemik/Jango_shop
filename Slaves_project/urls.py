91c186fa4fa03a8ac8ad1e4e220b207a5f84413a
LikoyakaTemik <92818915+LikoyakaTemik@users.noreply.github.com>
2022-05-20 18:51:21 +0300

    Add files via upload

--- Slaves_project/urls.py
+++ Slaves_project/urls.py
@@ -1,8 +1,21 @@
+# django_auth/urls.py
 
 from django.contrib import admin
-from django.urls import path
-from slave_site import views
+from django.urls import path, include
+from django.conf.urls import url
+from users import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index_page),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('home/', include('users.urls')),
+    path('', views.home, name="home"),
+    path("new_product/", views.new_product),
+    path('account/', views.account, name="account"),
+    path('shopping_cart/', views.shopping_cart, name="shopping_cart"),
+    path('sc/', views.add_sc, name="add_sc"),
+    path('room/', views.room, name="room"),
+    path('chat_input/', views.chat_input),
+    path('chat_list/', views.chat_list),
+    path('create_chat/', views.create_chat, name="create_chat"),
 ]
